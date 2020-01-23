6d23a4742d1ff6264f0cae202f0479592a36aa1d
Juan Negrier <juannegrier@gmail.com>
2020-01-23 12:11:25 +0000

    Loading data from Core Service on the frontend

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,65 @@ const useStyles = makeStyles((_: Theme) =>
   })
 )
 
-type Props = {}
+export interface TestProject {
+  id: number
+  name: string
+  project_status: string
+  data: { url?: string }
+}
+
+export interface TestLaunch {
+  id: number
+  name: string
+  launch_status: string
+  project: string
+  data: { url?: string }
+}
+
+export interface TestRun {
+  id: number
+  launch: string
+  start_datetime: string
+  end_datetime: string
+  test_run_status: string
+  test_type: string
+  data: { url?: string }
+}
+
+export interface TestSuite {
+  id: number
+  name: string
+  start_datetime: string
+  end_datetime: string
+  test_suite_status: string
+  test_type: string
+  data: { url?: string }
+  test_run: {
+    id: number
+    launch: string
+    test_run_status: string
+    test_type: string
+  }
+}
+
+export interface Test {
+  id: number
+  name: string
+  start_datetime: string
+  end_datetime: string
+  test_resolution: string
+  test_status: string
+  test_suite: string
+  data: { url?: string }
+}
+
+type Props = {
+  test_projects: TestProject[]
+  test_launches: TestLaunch[]
+  test_runs: TestRun[]
+  test_suites: TestSuite[]
+  tests: Test[]
+}
 
 function Index(props: Props) {
   const classes = useStyles(props)
@@ -62,15 +120,112 @@ function Index(props: Props) {
             </Link>
           </Typography>
         </SpacingPaper>
-        <ul>
-          {/* {props.projects.map(project => (
-            <li key={project.id}>
-              <Link href="/p/[id]" as={`/p/${project.id}`}>
-                <a>{project.name}</a>
-              </Link>
-            </li>
-          ))} */}
-        </ul>
+
+        <SpacingPaper noPadding>
+          <Typography variant="h5">Projects</Typography>
+          {props.test_projects.map(project => (
+            <Typography variant="h6">
+              {project.id}
+              <br/>
+              {project.name}
+              <br/>
+              {project.project_status}
+              <br/>
+              {project.data.url}
+            </Typography>
+          ))}
+        </SpacingPaper>
+
+        <SpacingPaper noPadding>
+          <Typography variant="h5">Launches</Typography>
+          {props.test_launches.map(testLaunch => (
+            <Typography variant="h6">
+              {testLaunch.id}
+              <br/>
+              {testLaunch.name}
+              <br/>
+              {testLaunch.launch_status}
+              <br/>
+              {testLaunch.project}
+              <br/>
+              {testLaunch.data.url}
+            </Typography>
+          ))}
+        </SpacingPaper>
+
+        <SpacingPaper noPadding>
+          <Typography variant="h5">Runs</Typography>
+          {props.test_runs.map(testRun => (
+            <Typography variant="h6">
+              {testRun.id}
+              <br/>
+              {testRun.launch}
+              <br/>
+              {testRun.start_datetime}
+              <br/>
+              {testRun.end_datetime}
+              <br/>
+              {testRun.test_run_status}
+              <br/>
+              {testRun.test_type}
+              <br/>
+              {testRun.data.url}
+            </Typography>
+          ))}
+        </SpacingPaper>
+
+        <SpacingPaper noPadding>
+          <Typography variant="h5">Suites</Typography>
+          {props.test_suites.map(testSuite => (
+            <Typography variant="h6">
+              {testSuite.id}
+              <br/>
+              {testSuite.name}
+              <br/>
+              {testSuite.start_datetime}
+              <br/>
+              {testSuite.end_datetime}
+              <br/>
+              {testSuite.test_suite_status}
+              <br/>
+              {testSuite.test_type}
+              <br/>
+              {testSuite.data.url}
+              <br/>
+              {testSuite.test_run.id}
+              <br/>
+              {testSuite.test_run.launch}
+              <br/>
+              {testSuite.test_run.test_run_status}
+              <br/>
+              {testSuite.test_run.test_type}
+              <br/>
+            </Typography>
+          ))}
+        </SpacingPaper>
+        
+        <SpacingPaper noPadding>
+          <Typography variant="h5">Tests</Typography>
+          {props.tests.map(test => (
+            <Typography variant="h6">
+              {test.id}
+              <br/>
+              {test.name}
+              <br/>
+              {test.start_datetime}
+              <br/>
+              {test.end_datetime}
+              <br/>
+              {test.test_resolution}
+              <br/>
+              {test.test_status}
+              <br/>
+              {test.test_suite}
+              <br/>
+              {test.data.url}
+            </Typography>
+          ))}
+        </SpacingPaper>
       </HeaderArticleContainer>
     </Layout>
   )
@@ -81,17 +236,37 @@ function Index(props: Props) {
  */
 Index.getInitialProps = async (ctx: AppContext): Promise<Props> => {
   const { store } = ctx
-  const res = await fetch("http://delta_core_service:5000/get_projects", {
+  const projectReq = await fetch(
+    "http://delta_core_service:5000/get_projects",
+    {
     method: "POST",
   })
-  const data = await res.json()
+  const projects = await projectReq.json()
 
-  console.log(`Show data fetched. Count: ${data.length}`)
-  console.log(`Show data fetched. Count: ${data}`)
-  console.log(`Show data fetched. Count: ${data.id}`)
-  console.log(`Show data fetched. Count: ${data.name}`)
-  console.log(`Show data fetched. Count: ${data.data}`)
-  console.log(`Show data fetched. Count: ${data.project_status}`)
+  const launchesReq = await fetch(
+    "http://delta_core_service:5000/get_launches",
+    {
+    method: "POST",
+  })
+  const launches = await launchesReq.json()
+
+  const runsReq = await fetch("http://delta_core_service:5000/get_test_runs", {
+    method: "POST",
+  })
+  const runs = await runsReq.json()
+
+  const suitesReq = await fetch(
+    "http://delta_core_service:5000/get_test_suites",
+    {
+    method: "POST",
+    }
+  )
+  const suites = await suitesReq.json()
+
+  const testsReq = await fetch("http://delta_core_service:5000/get_tests", {
+    method: "POST",
+  })
+  const tests = await testsReq.json()
 
   const pagePayload: IPagePayload = {
     selectedPage: Page.TOP,
@@ -101,7 +276,11 @@ Index.getInitialProps = async (ctx: AppContext): Promise<Props> => {
     payload: pagePayload,
   })
   return {
-    projects: data.map(project => project.data)
+    test_projects: projects,
+    test_launches: launches,
+    test_runs: runs,
+    test_suites: suites,
+    tests: tests
   }
 }
 
