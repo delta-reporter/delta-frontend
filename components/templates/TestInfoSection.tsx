e694e7decd352117dd647dc3f6f7d2144f7a4ef5
opishcheiko <oleksandra.pishcheiko@distilled.ie>
2020-06-08 17:53:48 +0100

    trying to fetch data (get)

--- components/templates/TestInfoSection.tsx
+++ components/templates/TestInfoSection.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { makeStyles } from "@material-ui/core/styles"
 import {
   Paper,
@@ -174,29 +174,38 @@ export const TestInfoSection = function(props: TestProps) {
     setSelectedResolutionValue(value)
   }
 
-  function SetTestResolution(props: ResolutionProps, testHistoryId) {
+  const setTestResolution = (props: ResolutionProps, testHistoryId) => {
     const { onClose, selectedValue, open } = props
 
     const handleClose = () => {
       onClose(selectedValue)
     }
 
-    const handleListItemClick = async (resolution: string) => {
+    async function handleListItemClick(resolution: string) {
       // POST request using fetch with async/await
-      const requestOptions = {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          test_history_id: testHistoryId,
-          test_resolution: resolution,
-        }),
-      }
-      const response = await fetch(
-        `${process.env.deltaCore}/api/v1/test_history_resolution`,
-        requestOptions
-      )
-      const data = await response.json()
-      onClose(data)
+
+      const url = "http://localhost:5000/api/v1/test_history_resolution" // site that doesn’t send Access-Control-*
+      fetch(url)
+        .then(response => response.text())
+        .then(contents => console.log(contents))
+        .catch(() =>
+          console.log("Can’t access " + url + " response. Blocked by browser?")
+        )
+
+      // const requestOptions = {
+      //   method: "POST",
+      //   headers: { "Content-Type": "application/json" },
+      //   body: JSON.stringify({
+      //     test_history_id: testHistoryId,
+      //     test_resolution: resolution,
+      //   }),
+      // }
+      // const response = await fetch(
+      //   `http://localhost:5000/api/v1/test_history_resolution`,
+      //   requestOptions
+      // )
+      // const data = await response.json()
+      // onClose(data)
     }
 
     return (
@@ -226,87 +235,143 @@ export const TestInfoSection = function(props: TestProps) {
     setHistoryTabValue(newValue)
   }
 
+  // TEST
+
+  // function setResponce(responce) {
+  //   return <h1>This is your data {responce}</h1>
+  // }
+
+  // function componentDidMount() {
+  //   const apiUrl = "https://delta-core.dsch.dev/api/v1/projects"
+  //   let responce
+  //   fetch(apiUrl)
+  //     .then(response => response.json())
+  //     .then(data => setResponce(data))
+  // }
+
+  // TEST
+
+  function handleResolutionLoading(Component) {
+    return function handleResolutionLoadingComponent({ isLoading, ...props }) {
+      if (!isLoading) return <Component {...props} />
+      return (
+        <p style={{ textAlign: "center", fontSize: "30px" }}>
+          Hold on, fetching data may take some time :)
+        </p>
+      )
+    }
+  }
+
+  const showTheResolutionResponse = function(props) {
+    const { resolution } = props
+    // this is for when the page loads and the call for resolution haven't been made yet
+    if (!resolution || resolution.length === 0) {
+      return <p>No resolution available</p>
+    }
+    return <p>{resolution[0].name} </p>
+  }
+
+  const LoadResolution = handleResolutionLoading(showTheResolutionResponse)
+  const [appState, setAppState] = useState({
+    loading: false,
+    resolution: null,
+  })
+
+  useEffect(() => {
+    const apiUrl = `https://delta-core.dsch.dev/api/v1/projects`
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(resolutionResponse => {
+        setAppState({ loading: false, resolution: resolutionResponse })
+      })
+  }, [setAppState])
+
   return (
     <div key={children.test_history_id} className={classes.root}>
+      <LoadResolution
+        isLoading={appState.loading}
+        resolution={appState.resolution}
+      />
+
       <Typography className={classes.bigMargin}>
         Full path:
         <span style={{ color: "grey" }}> {children.name}</span>
       </Typography>
-      {children.message ? ( // if there is any error message - show the info, else - test passed
-        <Paper className={classes.paperNoPadding} elevation={0}>
-          <AppBar
-            style={{ backgroundColor: "white", border: "none" }}
-            variant="outlined"
-            position="relative"
-            className={classes.tabs}
+      {/* {children.message ? ( // if there is any error message - show the info, else - test passed */}
+      <Paper className={classes.paperNoPadding} elevation={0}>
+        <AppBar
+          style={{ backgroundColor: "white", border: "none" }}
+          variant="outlined"
+          position="relative"
+          className={classes.tabs}
+        >
+          <Tabs
+            value={historyTabValue}
+            onChange={handleTabChange}
+            indicatorColor="primary"
+            textColor="secondary"
+            variant="fullWidth"
+            aria-label="full width tabs example"
           >
-            <Tabs
-              value={historyTabValue}
-              onChange={handleTabChange}
-              indicatorColor="primary"
-              textColor="secondary"
-              variant="fullWidth"
-              aria-label="full width tabs example"
+            <Tab label="Error" {...a11yProps(0)} />
+            <Tab label="Test History" {...a11yProps(1)} />
+          </Tabs>
+        </AppBar>
+        <TabPanel value={historyTabValue} index={0}>
+          <ErrorMessagePanel
+            key={children.test_history_id}
+            expanded={expandedErrorMessage === children.message}
+            onChange={expandCollapseErrorMessage(children.message)}
+            TransitionProps={{ unmountOnExit: true }}
+          >
+            <ErrorMessageCollapsedLineSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel3bh-content"
             >
-              <Tab label="Error" {...a11yProps(0)} />
-              <Tab label="Test History" {...a11yProps(1)} />
-            </Tabs>
-          </AppBar>
-          <TabPanel value={historyTabValue} index={0}>
-            <ErrorMessagePanel
-              key={children.test_history_id}
-              expanded={expandedErrorMessage === children.message}
-              onChange={expandCollapseErrorMessage(children.message)}
-              TransitionProps={{ unmountOnExit: true }}
-            >
-              <ErrorMessageCollapsedLineSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel3bh-content"
+              <Typography color="textPrimary">{children.message}</Typography>
+            </ErrorMessageCollapsedLineSummary>
+            <ErrorMessagePanelDetails>
+              <List>
+                <ListItem button>
+                  {" "}
+                  {children.error_type}
+                  {children.trace}
+                  {children.retries}
+                </ListItem>
+              </List>
+            </ErrorMessagePanelDetails>
+          </ErrorMessagePanel>
+          <div>
+            <Typography className={classes.bigMargin}>
+              <Button
+                // disabled
+                variant="outlined"
+                color="primary"
+                onClick={handleResolutionDialogOpen}
+                className={classes.bigMargin}
               >
-                <Typography color="textPrimary">{children.message}</Typography>
-              </ErrorMessageCollapsedLineSummary>
-              <ErrorMessagePanelDetails>
-                <List>
-                  <ListItem button>
-                    {" "}
-                    {children.error_type}
-                    {children.trace}
-                    {children.retries}
-                  </ListItem>
-                </List>
-              </ErrorMessagePanelDetails>
-            </ErrorMessagePanel>
-            <div>
-              <Typography className={classes.bigMargin}>
-                <Button
-                  disabled
-                  variant="outlined"
-                  color="primary"
-                  onClick={handleResolutionDialogOpen}
-                  className={classes.bigMargin}
-                >
-                  Set test resolution
-                </Button>{" "}
-                <span style={{ marginLeft: "15px" }}>Selected: </span>
-                <span style={{ color: "grey", fontStyle: "italic" }}>
-                  {selectedResolutionValue}
-                </span>
-              </Typography>
-              <SetTestResolution
-                open={openResolutionDialog}
-                selectedValue={selectedResolutionValue}
-                onClose={handleResolutionDialogClose}
-                {...children.test_history_id}
-              />
-            </div>
-          </TabPanel>
-          <TabPanel value={historyTabValue} index={1}>
-            TODO: Historical info for this test {children.test_id}
-          </TabPanel>
-        </Paper>
-      ) : (
-        <div></div>
-      )}
+                Set test resolution
+              </Button>{" "}
+              <span style={{ marginLeft: "15px" }}>Selected: </span>
+              <span style={{ color: "grey", fontStyle: "italic" }}>
+                {selectedResolutionValue}
+              </span>
+            </Typography>
+            {/* <setTestResolution
+              open={openResolutionDialog}
+              selectedValue={selectedResolutionValue}
+              onClose={handleResolutionDialogClose}
+              {...children.test_history_id}
+            /> */}
+          </div>
+        </TabPanel>
+        <TabPanel value={historyTabValue} index={1}>
+          TODO: Historical info for this test {children.test_id}
+        </TabPanel>
+      </Paper>
+      {/* ) : ( */}
+      {/* <div></div> */}
+      {/* )} */}
     </div>
   )
 }
