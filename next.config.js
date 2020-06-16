030893c164b602ffdc72fd84a8a451ea7628b54e
opishcheiko <oleksandra.pishcheiko@distilled.ie>
2020-06-16 17:41:46 +0100

    clean up

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ const withCSS = require("@zeit/next-css")
 module.exports = withCSS({
     target: 'serverless',
     env: {
-      deltaCore: process.env.DELTA_CORE_URL, // getting these env vars from docker-compose
+      deltaCore: process.env.DELTA_CORE_URL,
     },
   })
