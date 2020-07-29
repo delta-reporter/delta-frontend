ad3d6c033880a01c1746d0ac400fcf898fc080f6
Alex Pishcheiko <oleksandra.pishcheiko@distilled.ie>
2020-07-29 09:14:27 +0100

    fixing unit test

--- pages/tests/[testsByRunId].tsx
+++ pages/tests/[testsByRunId].tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
 import fetch from "isomorphic-unfetch"
 import { makeStyles } from "@material-ui/core/styles"
 import { SuiteAndTest } from "../index"
