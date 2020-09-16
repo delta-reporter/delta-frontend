917bba8140b34d9693b40541ca017de342458752
Juan Negrier <juannegrier@gmail.com>
2020-09-16 13:02:38 +0100

    Removing unused imports

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,9 +1,6 @@
 import { Typography, Container } from "@material-ui/core"
 import { makeStyles } from "@material-ui/core/styles"
 import React from "react"
-import { AppContext } from "../components/AppContext"
-import { Page } from "../constants"
-import { IPagePayload, PageActions } from "../store/page"
 import { BasePage } from "../components/templates/BasePage"
 
 const useStyles = makeStyles(theme => ({
@@ -31,24 +28,6 @@ function Error(props: Props) {
       </Container>
     </BasePage>
   )
-}
-
-/**
- * Server side rendering
- */
-Error.getInitialProps = async (ctx: AppContext): Promise<Props> => {
-  const { res, store } = ctx
-
-  const pagePayload: IPagePayload = {
-    selectedPage: Page.ERROR,
-  }
-  store.dispatch({
-    type: PageActions.changePage.toString(),
-    payload: pagePayload,
-  })
-  return {
-    httpStatusCode: res.statusCode,
-  }
 }
 
 export default Error
