4754645161395ac4af7e05f5bf57de434d6d0e93
Juan Negrier <juannegrier@gmail.com>
2020-09-16 12:44:00 +0100

    Removing Redux store

--- store/page/selectors.ts
+++
@@ -1,4 +0,0 @@
-import { IInitialState } from "../states"
-
-export const selectedPageSelector = (state: IInitialState) =>
-  state.page.selectedPage
--- store/sagas.ts
+++
@@ -1,6 +0,0 @@
-import { all, fork } from "redux-saga/effects"
-import { watchFetchDebounce, watchFetchThrottle } from "./redux-saga/sagas"
-
-export const rootSaga = function* root() {
-  yield all([fork(watchFetchDebounce), fork(watchFetchThrottle)])
-}
