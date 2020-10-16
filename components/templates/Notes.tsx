d80c2ed72f935ee0638c773c9ac5186b2786b5fe
Alex Pishcheiko <oleksandra.pishcheiko@distilled.ie>
2020-10-16 18:37:27 +0100

    updating the notes using useEffect

--- components/templates/Notes.tsx
+++ components/templates/Notes.tsx
@@ -1,9 +1,6 @@
 import { useState, useEffect } from 'react';
-import produce from 'immer';
 import { Button, Input } from '@material-ui/core';
-import useSWR from 'swr';
 
-const fetcher = url => fetch(url).then(res => res.json())
 
 interface NotesProps {
     mother_test_id: number
@@ -12,18 +9,22 @@ interface NotesProps {
 
 export const Notes = function(props: NotesProps) {
     const { mother_test_id, darkMode} = props;
-    const initialData = [{ text: 'Loading Notes ... ' }];
-    const [data, setData] = useState(initialData);
-    const { data, error } = useSWR(
-      `${process.env.publicDeltaCore}/api/v1/notes/${mother_test_id}`,
-      fetcher
-    )
-    const [notes, setNotes] = useState(data);
+    const [notes, setNotes] = useState([]);
 
-    const loading = !notes && !error
-    const noData = !data
+    const loading = !notes
+    const noData = notes.message // if there are no notes, we get {"message": "No notes were found"}
+
+    useEffect(() => {
+      const getData = async () => {
+        const response = await fetch(`${process.env.publicDeltaCore}/api/v1/notes/${mother_test_id}`);
+        const data = await response.json();
+        setNotes(data);
+      };
+      getData();
+    }, [])
 
     const addNote = async (noteText) => {
+      // POST request to add a new note
       const requestOptions = {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -40,7 +41,8 @@ export const Notes = function(props: NotesProps) {
         requestOptions
       )
       await response.json()
-
+      
+      //GET request to refresh the list of all notes
       const requestOptions2 = {
         method: "GET",
         headers: { "Content-Type": "application/json" }
@@ -55,85 +57,31 @@ export const Notes = function(props: NotesProps) {
       setNotes(res)
     }
 
-    function getBackgroundColor(darkMode) {
-        if(darkMode) return "#2a2a2a"
-        else return "white"
-    }
+    // function getBackgroundColor(darkMode) {
+    //     if(darkMode) return "#2a2a2a"
+    //     else return "white"
+    // }
     
-    function getTextColor(darkMode) {
-        if(darkMode) return "#8c8d8d"
-        else return "black"
-    }
-
-    // const addNote = async (noteText) => {
-    //     const requestOptions = {
-    //       method: "POST",
-    //       headers: { "Content-Type": "application/json" },
-    //       body: JSON.stringify({
-    //         test_id: testId,
-    //         test_resolution: resolutionValue,
-    //         mother_test_id: motherTestId
-    //       }),
-    //     }
-    //     console.log(requestOptions)
-    
-    //     const response = await fetch(
-    //       `${process.env.publicDeltaCore}/api/v1/test_history_resolution`,
-    //       requestOptions
-    //     )
-    //     await response.json()
-    //   }
+    // function getTextColor(darkMode) {
+    //     if(darkMode) return "#8c8d8d"
+    //     else return "black"
+    // }
   
-    // const handleClick = () => {
-    //   const text = document.querySelector('#noteinput').value.trim();
-    //   if (text) {
-    //     const nextState = produce(notes, draftState => {
-    //       draftState.push({ text });
-    //     });
-    //     document.querySelector('#noteinput').value = '';
-  
-    //     if (typeof window !== 'undefined') {
-    //       localStorage.setItem('data', JSON.stringify(nextState));
-    //     }
-  
-    //     setNotes(nextState);
-    //   }
-    // };
-  
-    // useEffect(() => {
-    //   if (typeof window !== 'undefined') {
-    //     const getData = localStorage.getItem('data');
-  
-    //     if (getData !== '' && getData !== null) {
-    //       return setNotes(JSON.parse(getData));
-    //     }
-    //     return setNotes([]);
-    //   }
-    // }, 0);
-  
-    if(noData) {
-      return (
-        <>
-        <p>No data</p>
-        </>
-      )
-    } else {
-      return (
-        <>
+    return (
+      <>
           <Input id="noteinput" style={{ width: '80%' }} type="text" placeholder="Enter a new note" />
           <Button onClick={() => addNote((document.getElementById("noteinput") as HTMLInputElement).value)}>Add note</Button>
-          {loading
-            ? "Loading notes..."
-            : notes.map(note => (
+          {loading || noData ?
+           <p>There are no notes for this test. Feel free to add one :) </p>
+          : (
+            notes.map(note => (
               <>
               <div>{note.note_text}</div>
               <div>{note.added_by}</div>
               <div>{note.created_datetime}</div>
               </>
               ))
-            }
-        </>
-      );
-    }
-
+          )} 
+      </>
+    )
 }
