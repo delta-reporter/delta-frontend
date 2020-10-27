fb6871206c226dce77bd0e635f1c4abd421127b8
cristianmoisa <cristianmoisa@ymail.com>
2020-10-27 10:18:32 +0000

    Notes style changes

--- components/templates/Notes.tsx
+++ components/templates/Notes.tsx
@@ -1,6 +1,16 @@
 import { useState, useEffect } from 'react';
-import { Button, Input } from '@material-ui/core';
+import { Button, FormControl, List, makeStyles, TextField } from '@material-ui/core';
 
+const useStyles = makeStyles(theme => ({
+  addNoteButton: {
+      backgroundColor: theme.palette.primary.main,
+      width: "90px",
+      height: "30px",
+      marginTop: "5px",
+      border: "1px #a7bab1 solid",
+      fontSize: "12px",
+  }
+}))
 
 interface NotesProps {
     mother_test_id: number
@@ -8,80 +18,86 @@ interface NotesProps {
 }
 
 export const Notes = function(props: NotesProps) {
-    const { mother_test_id, darkMode} = props;
+    const classes = useStyles(props)
+    const { mother_test_id} = props;
     const [notes, setNotes] = useState([]);
 
     const loading = !notes
     const noData = notes.message // if there are no notes, we get {"message": "No notes were found"}
 
+    const getData = async () => {
+      const response = await fetch(`${process.env.publicDeltaCore}/api/v1/notes/${mother_test_id}`);
+      const data = await response.json();
+      setNotes(data);
+    };
+
     useEffect(() => {
-      const getData = async () => {
-        const response = await fetch(`${process.env.publicDeltaCore}/api/v1/notes/${mother_test_id}`);
-        const data = await response.json();
-        setNotes(data);
-      };
       getData();
     }, [])
 
-    const addNote = async (noteText) => {
+    const getNotes = async (mother_test_id: number) => {
+      //GET existing notes for test with id
+      const options = {
+        method: "GET",
+        headers: { "Content-Type": "application/json" }
+      };
+      const getNotesResponse = await fetch(
+        `${process.env.publicDeltaCore}/api/v1/notes/${mother_test_id}`,
+        options
+      );
+      return await getNotesResponse.json();
+    }
+
+    const addNote = async (noteText, addedBy) => {
       // POST request to add a new note
-      const requestOptions = {
+      const options = {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
           mother_test_id: mother_test_id,
           note_text: noteText,
-          added_by: "Johnny Cristian"
+          added_by: addedBy
         }),
       }
-      console.log(requestOptions)
-  
-      const response = await fetch(
+      const postResponse = await fetch(
         `${process.env.publicDeltaCore}/api/v1/notes`,
-        requestOptions
+        options
       )
-      await response.json()
-      
-      //GET request to refresh the list of all notes
-      const requestOptions2 = {
-        method: "GET",
-        headers: { "Content-Type": "application/json" }
-      }
-  
-      const response2 = await fetch(
-        `${process.env.publicDeltaCore}/api/v1/notes/${mother_test_id}`,
-        requestOptions2
-      )
-      const res = await response2.json()
+      await postResponse.json()
+      const res = await getNotes(mother_test_id);
 
-      setNotes(res)
+      (document.getElementById("noteText") as HTMLInputElement).value = '';
+      (document.getElementById("noteAddedBy") as HTMLInputElement).value = '';
+      setNotes(res);
     }
 
-    // function getBackgroundColor(darkMode) {
-    //     if(darkMode) return "#2a2a2a"
-    //     else return "white"
-    // }
-    
-    // function getTextColor(darkMode) {
-    //     if(darkMode) return "#8c8d8d"
-    //     else return "black"
-    // }
-  
+    const submit = () => {
+      addNote((document.getElementById("noteText") as HTMLInputElement).value, 
+          (document.getElementById("noteAddedBy") as HTMLInputElement).value)
+    }
+
     return (
       <>
-          <Input id="noteinput" style={{ width: '80%' }} type="text" placeholder="Enter a new note" />
-          <Button onClick={() => addNote((document.getElementById("noteinput") as HTMLInputElement).value)}>Add note</Button>
-          {loading || noData ?
-           <p>There are no notes for this test. Feel free to add one :) </p>
-          : (
-            notes.map(note => (
+      <FormControl style={{display: "flex"}}>
+          <TextField id="noteText" type="text" placeholder="Enter a new note" multiline autoFocus required/>
+          <TextField id="noteAddedBy"  type="text" placeholder="Enter your name"/>
+      </FormControl>
+      <Button id="submit" onClick={() => submit()} className={classes.addNoteButton}>Add note</Button>
+      <List style={{maxHeight: 200, overflow: 'auto'}}>
+        {loading || noData ?
+          <p>There are no notes for this test. Feel free to add one :) </p>
+            : (
+            notes.reverse().map(note => (
               <>
               <div>{note.note_text}</div>
-              <div>{note.added_by}</div>
-              <div>{note.created_datetime}</div>
+              <div style={{display: 'flex', color: 'grey'}}>
+                <div style={{paddingRight: '5px'}}>{note.added_by}</div>
+                <div>{note.created_datetime}</div>
+              </div>
               </>
               ))
           )} 
+          </List>
       </>
     )
 }
